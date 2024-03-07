6eebe4b64423671cbe8e1653ba604fb0b8bc3fc7
QAZebass <ze.basszender@gmail.com>
2024-03-07 19:10:13 -0300

    [LOGIN]Preparing files for new tests, deleted and modified config

--- pages/login.ts
+++ pages/login.ts
@@ -1,22 +1,29 @@
-import { expect, type Locator, type Page } from "@playwright/test"
+import { expect, Selectors, type Locator, type Page } from "@playwright/test"
 
 
 export class LoginPage {
     readonly page: Page;
-    readonly emailInputLogin: Locator;
-    readonly passwordInputLogin: Locator;
-    readonly loginButton: Locator
+    readonly wrapper: Locator;
+    readonly loginTitle: Locator;
+    readonly emailInput: Locator;
+    readonly passwordInput: Locator;
+    readonly submitButton: Locator
     
     constructor(page: Page){
         this.page = page
-        this.emailInputLogin = page.locator('[data-cy="emailInput"]')
-        this.passwordInputLogin = page.locator('[data-cy="passwordInput"]')
-        this.loginButton = page.locator('[data-cy="loginButton"]')
+        this.wrapper = page.locator('form')
+        this.loginTitle = page.locator('.main-content p').first()
+        this.emailInput = page.locator('input#email')
+        this.passwordInput = page.locator('input#password]')
+        this.submitButton = page.locator('button#submit]')
     }
 
     async login(email: string, password: string){
-        await this.emailInputLogin.fill(email)
-        await this.passwordInputLogin.fill(password)
-        await this.loginButton.click()
+        expect(this.loginTitle).toHaveText('Log In')
+        await this.emailInput.waitFor({state: 'visible'})
+        await this.emailInput.fill(email)
+        await this.passwordInput.fill(password)
+        await this.submitButton.waitFor()
+        await this.submitButton.click()
     }
 }
