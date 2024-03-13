1a76cf7eb00506692d05e8fce143b4c368137ee5
QAZebass <ze.basszender@gmail.com>
2024-03-13 08:56:36 -0300

    filed modified, registerpage, sign up test and locators

--- tests/[SIGN-UP]-ThinkingTester.spec.ts
+++ tests/[SIGN-UP]-ThinkingTester.spec.ts
@@ -1,4 +1,5 @@
 import { test } from '@playwright/test'
+import faker from '@faker-js/faker'
 import { LoginPage } from '../pages/loginPage';
 import { RegisterPage } from '../pages/registerPage';
 import { MethodsPage } from '../pages/methodsPage';
@@ -9,14 +10,17 @@ import { titles } from '../utils/helpers/dataFixture';
 test.describe('Sign Up', async ()=>{
 
     test('TC1: Validate that the user can register a new account', async ({page, baseURL})=>{
-        const signUpTitleTitle = titles.signUpTitle
+        const signUpTitle = titles.signUpTitle
         const registerTitle= titles.registerTitle
         const loginPage = new LoginPage(page)
         const registerPage = new RegisterPage(page)
+        
+
         await page.goto(`${baseURL}`)
-        await loginPage.assertTitle(signUpTitleTitle)
+        await loginPage.assertTitle(signUpTitle)
         await loginPage.clickOnSignUp()
         await registerPage.assertSignUpText(registerTitle)
+        await registerPage.typeInFirstName('Sebastian')
 
     })
 })
